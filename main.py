c9373c28899bc57645bc592f8de600ad212c850f
Clay <156083036+Clay551@users.noreply.github.com>
2024-08-06 04:57:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-from colorama import Fore , init
 import colorama
-import pyfiglet 
+from colorama import Fore
+import pyfiglet
 import subprocess
-import argparse
-import os
-print(colorama.Fore.RED)
-pyfiglet.print_figlet("Asylum")
-print(colorama.Fore.GREEN)
-print("             MAC Changer")
 
-print(colorama.Fore.RESET)
+colorama.init()
+
+print(Fore.RED)
+pyfiglet.print_figlet("Asylum")
+print(Fore.GREEN)
+print("             MAC Changer")
+print(Fore.RESET)
+
 def change_mac(interface, new_mac):
-    print(colorama.Fore.GREEN+"[+]Changing MAC for interface " + interface + " to " + new_mac)
-    print(colorama.Fore.RED)
+    print(Fore.GREEN + "[+] Changing MAC for interface " + interface + " to " + new_mac)
+    print(Fore.RED)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-
-interface = input("interface==> ")
-mac = input("MAC==>")
+interface = input("Interface ==> ")
+mac = input("MAC ==> ")
 
 change_mac(interface, mac)
